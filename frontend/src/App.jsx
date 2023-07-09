88d63292a2bd6dd1593e52fcdc3e69065224d86f
Agustin Villarroel <agustin@MacBook-Pro-de-Agustin.local>
2023-07-08 22:36:08 -0300

    google reCaptcha v3 implementado

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -18,45 +18,46 @@ import ProductosTotal from './components/ProductosTotal';
 import CartPage from './pages/CartPage';
 import ProductosCategoria from './components/ProductosCategoria';
 import Anuncio from './components/Anuncio';
+import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3';
 
 
 const App = () => {
-
-
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<div><Anuncio/><Navbar /><Home /><Footer /></div>}/>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/tienda" element={<div><Anuncio/><Navbar /><ProductosTotal /><Footer /></div>}/>
-        <Route path="/tienda/:categoriaId" element={<div><Anuncio/><Navbar /><ProductosCategoria /><Footer /></div>}/>
-        <Route path="/perfil" element={<div><Navbar /><Perfil /><Footer /></div>}/>
-        <Route path='/carrito' element={<div><Navbar /><Anuncio/><CartPage /><Footer /></div>}/>
-        <Route path="/404" element={<Error />} />
-        <Route path="*" element={<Navigate to="/404" />} />
-        <Route exact path='/dashboard' element={<PrivateRoute />}>
-          <Route exact path='/dashboard' element={<Dashboard/>}/>
-        </Route>
-        <Route exact path='/dashboard/inventario' element={<PrivateRoute/>}>
-          <Route exact path='/dashboard/inventario' element={<Inventory/>}/>
-        </Route>
-        <Route exact path='/dashboard/inventario/agregar' element={<PrivateRoute/>}>
-          <Route exact path='/dashboard/inventario/agregar' element={<NewProduct/>}/>
-        </Route>
-        <Route exact path='/dashboard/inventario/actualizar/:id' element={<PrivateRoute/>}>
-          <Route exact path='/dashboard/inventario/actualizar/:id' element={<UpdateProduct />} />
-        </Route>
-        <Route exact path='/dashboard/clientes' element={<PrivateRoute/>}>
-          <Route exact path='/dashboard/clientes' element={<Clientes/>}/>
-        </Route>
-        <Route exact path='/dashboard/categorias' element={<PrivateRoute/>}>
-          <Route exact path='/dashboard/categorias' element={<NCategoria/>}/>
-          <Route exact path='/dashboard/categorias/:id' element={<NCategoria/>}/>
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <GoogleReCaptchaProvider reCaptchaKey="6LfxqAknAAAAADX06900xrbOBnjIZGUjcq7YQEZ4">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<div><Anuncio /><Navbar /><Home /><Footer /></div>} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/tienda" element={<div><Anuncio /><Navbar /><ProductosTotal /><Footer /></div>} />
+          <Route path="/tienda/:categoriaId" element={<div><Anuncio /><Navbar /><ProductosCategoria /><Footer /></div>} />
+          <Route path="/perfil" element={<div><Navbar /><Perfil /><Footer /></div>} />
+          <Route path="/carrito" element={<div><Navbar /><Anuncio /><CartPage /><Footer /></div>} />
+          <Route path="/404" element={<Error />} />
+          <Route path="*" element={<Navigate to="/404" />} />
+          <Route exact path="/dashboard" element={<PrivateRoute />}>
+            <Route exact path="/dashboard" element={<Dashboard />} />
+          </Route>
+          <Route exact path="/dashboard/inventario" element={<PrivateRoute />}>
+            <Route exact path="/dashboard/inventario" element={<Inventory />} />
+          </Route>
+          <Route exact path="/dashboard/inventario/agregar" element={<PrivateRoute />}>
+            <Route exact path="/dashboard/inventario/agregar" element={<NewProduct />} />
+          </Route>
+          <Route exact path="/dashboard/inventario/actualizar/:id" element={<PrivateRoute />}>
+            <Route exact path="/dashboard/inventario/actualizar/:id" element={<UpdateProduct />} />
+          </Route>
+          <Route exact path="/dashboard/clientes" element={<PrivateRoute />}>
+            <Route exact path="/dashboard/clientes" element={<Clientes />} />
+          </Route>
+          <Route exact path="/dashboard/categorias" element={<PrivateRoute />}>
+            <Route exact path="/dashboard/categorias" element={<NCategoria />} />
+            <Route exact path="/dashboard/categorias/:id" element={<NCategoria />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </GoogleReCaptchaProvider>
   );
-}
+};
 
 export default App;
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -4,6 +4,7 @@ import axios from 'axios';
 import { GiCrystalGrowth } from 'react-icons/gi';
 import { toast, Toaster } from 'react-hot-toast';
 import jwtDecode from 'jwt-decode';
+import { GoogleReCaptchaProvider, useGoogleReCaptcha } from 'react-google-recaptcha-v3';
 
 const Login = () => {
     const [values, setValues] = useState({
@@ -13,24 +14,29 @@ const Login = () => {
     const navigate = useNavigate();
 
     axios.defaults.withCredentials = true;
-    const handleSubmit = (e) => {
+
+    const { executeRecaptcha } = useGoogleReCaptcha();
+
+    const handleSubmit = async (e) => {
         e.preventDefault();
+        const token = await executeRecaptcha('Login');
+        console.log(token)
         axios
-            .post('http://localhost:8000/login', values)
+            .post('http://localhost:8000/login', { ...values, token })
             .then((res) => {
-                if (res.data.Status === "Perfecto") {
+                if (res.data.Status === 'Perfecto') {
                     // Obtener el token del response
                     const token = res.data.token;
                     // Decodificar el token para obtener la información
                     const decodedToken = jwtDecode(token);
                     // Verificar si el usuario es administrador
                     if (decodedToken.isAdmin) {
-                        toast.success("Ingresaste correctamente administrador");
+                        toast.success('Ingresaste correctamente administrador');
                         setTimeout(() => {
                             navigate('/dashboard');
                         }, 1000);
                     } else {
-                        toast.success("Ingresaste correctamente");
+                        toast.success('Ingresaste correctamente');
                         setTimeout(() => {
                             navigate('/');
                         }, 1000);
@@ -44,41 +50,63 @@ const Login = () => {
             .catch((err) => console.log(err));
     };
 
-
     return (
         <div className="flex h-screen bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 font-primary justify-center items-center ">
             <Toaster />
             <div className="w-full max-w-md m-auto bg-white shadow-xl rounded-md p-10">
-                <header className='text-center pt-10 pb-20'>
-                    <a className='text-4xl font-bold text-center text-purple-600'>PatagoniaGems</a>
+                <header className="text-center pt-10 pb-20">
+                    <a className="text-4xl font-bold text-center text-purple-600">PatagoniaGems</a>
                 </header>
-                <form onSubmit={handleSubmit}>
-                    <div className="">
-                        
-                    </div>
-                    <div>
-                        <label className="block mb-2 text-purple-600 float-left">Correo</label>
-                        <input required className="w-full p-2 mb-6 text-purple-600 border-b-2 border-gray-300 outline-none" type="email" onChange={e => setValues({ ...values, email: e.target.value })} placeholder="Escriba su correo electrónico" />
-                    </div>
-                    <div>
-                        <label className="block mb-2 text-purple-600">Contraseña</label>
-                        <input required className="w-full p-2 mb-6 text-purple-600 border-b-2 border-gray-300 outline-none" type="password" onChange={e => setValues({ ...values, contraseña: e.target.value })} placeholder='Escriba su contraseña' />
-                    </div>
-                    <div className='flex items-center justify-between'>
-                        <Link to="/"><GiCrystalGrowth size={35} className='hover:scale-125 text-purple-600 hover:text-red-600 mb-2 transition duration-300 transform hover:rotate-45' /></Link>
-                        <Link to="/" className="text-gray-500 hover:text-pink-700 text-sm">Olvidaste la contraseña?</Link>
-                    </div>
-                    <div className='m-2 items-center pt-4'>
-                        <button className="w-full bg-indigo-500 hover:bg-pink-500 duration-500 transition-all ease-in text-white font-bold py-2 px-4 mb-6 rounded-full " type="submit">Entrar</button>
-                    </div>
-                    <div className='flex flex-col text-center pt-20'>
-                        <a className='text-gray-500'>No tienes cuenta?</a>
-                        <Link to="/register" className="text-purple-500 hover:text-pink-700 text-sm ">Registrarse</Link>
-                    </div>
-                </form>
+                <GoogleReCaptchaProvider reCaptchaKey="6LfxqAknAAAAADX06900xrbOBnjIZGUjcq7YQEZ4">
+                    <form onSubmit={handleSubmit}>
+                        <div className=""></div>
+                        <div>
+                            <label className="block mb-2 text-purple-600 float-left">Correo</label>
+                            <input
+                                required
+                                className="w-full p-2 mb-6 text-purple-600 border-b-2 border-gray-300 outline-none"
+                                type="email"
+                                onChange={(e) => setValues({ ...values, email: e.target.value })}
+                                placeholder="Escriba su correo electrónico"
+                            />
+                        </div>
+                        <div>
+                            <label className="block mb-2 text-purple-600">Contraseña</label>
+                            <input
+                                required
+                                className="w-full p-2 mb-6 text-purple-600 border-b-2 border-gray-300 outline-none"
+                                type="password"
+                                onChange={(e) => setValues({ ...values, contraseña: e.target.value })}
+                                placeholder="Escriba su contraseña"
+                            />
+                        </div>
+                        <div className="flex items-center justify-between">
+                            <Link to="/">
+                                <GiCrystalGrowth
+                                    size={35}
+                                    className="hover:scale-125 text-purple-600 hover:text-red-600 mb-2 transition duration-300 transform hover:rotate-45"
+                                />
+                            </Link>
+                            <Link to="/" className="text-gray-500 hover:text-pink-700 text-sm">
+                                Olvidaste la contraseña?
+                            </Link>
+                        </div>
+                        <div className="m-2 items-center pt-4">
+                            <button className="w-full bg-indigo-500 hover:bg-pink-500 duration-500 transition-all ease-in text-white font-bold py-2 px-4 mb-6 rounded-full" type="submit">
+                                Entrar
+                            </button>
+                        </div>
+                        <div className="flex flex-col text-center pt-20">
+                            <a className="text-gray-500">No tienes cuenta?</a>
+                            <Link to="/register" className="text-purple-500 hover:text-pink-700 text-sm">
+                                Registrarse
+                            </Link>
+                        </div>
+                    </form>
+                </GoogleReCaptchaProvider>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Login
+export default Login;
